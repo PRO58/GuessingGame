4111b90ca371c1b577469250acedea90a5f6623b
PRO58 <46244541+PRO58@users.noreply.github.com>
2024-08-09 02:55:39 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//Simple random guessing number game;
+int main()
+{
+3
+    int input;
+    srand(time(NULL));
+    int r = rand() % 6;
+    while(input != r){
+        printf("Enter a number to guess 1-5: ");
+        scanf("%d", &input);
+
+
+
+    }
+    printf("You have guessed the number correctly");
+
+
+}
